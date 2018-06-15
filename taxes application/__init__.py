968cc115dd59f0e7c517ca1a20f1cad5471f69e7
Nataliya Sashnikova <sashnat@yahoo.com>
2018-06-15 23:15:09 +0300

    taxes calculation app

---
+++ taxes application/__init__.py
@@ -0,0 +1,1 @@
+#from taxes applications import *
---
+++ taxes application/taxes main.py
@@ -0,0 +1,53 @@
+# app for calculating taxes
+# functions + pandas
+# coded by Nataliya Sashnikova
+
+
+def compare(income):
+    i = input('input money, local currency: ')
+    j = income(float(i))
+    print ('left, $:', j)
+    got.append(i)
+    input_number.append("i")
+    left.append(int(j))
+    if len(input_number) >= 3:
+        print ('end')
+    else:
+        return compare(income)
+
+
+if __name__ == '__main__':
+    from taxes import income, z
+import pandas as pd
+got = []
+input_number = []
+left = []
+compare(income)
+df = pd.DataFrame({'input number': [i for i in range(1, len(input_number) + 1)],
+                   'local currency got': got,
+                   'left, $': left,
+                   'tax': [z for i in range(1, len(input_number) + 1)]
+                  })
+df.to_excel('C:\\Users\\user\PycharmProjects\\applications\\taxes application\\taxes.xlsx')   # file contains rest of excel files
+print(df)
+
+'''
+#-----------------------output-------------------------------------------------
+input currency exchange rate: 8
+input taxes: 18
+input money, local currency: 700000
+taxes, $: 15750.0
+left, $: 71750.0
+input money, local currency: 800000
+taxes, $: 18000.0
+left, $: 82000.0
+input money, local currency: 900000
+taxes, $: 20250.0
+left, $: 92250.0
+end
+   input number  left, $ local currency got   tax
+0             1    71750             700000  18.0
+1             2    82000             800000  18.0
+2             3    92250             900000  18.0
+
+'''

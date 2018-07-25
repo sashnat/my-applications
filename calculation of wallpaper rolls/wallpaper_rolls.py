da3130483cfc29ad9d03a5cdd537f965f70c798f
Nataliya Sashnikova <sashnat@yahoo.com>
2018-07-25 17:23:10 +0300

    dictionary+function+pandas

--- calculation of wallpaper rolls/wallpaper_rolls.py
+++ calculation of wallpaper rolls/wallpaper_rolls.py
@@ -5,7 +5,8 @@
 import math
 from functools import reduce
 import pandas as pd
+import numpy as np
 
-h_roll = float(input('высота (длина) рулона: '))
-w_roll = float(input('ширина рулона: '))
+h_roll = float(input('высота (длина) рулона/ roll  height: '))
+w_roll = float(input('ширина рулона/ roll width: '))
 

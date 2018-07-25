75f6d813ed5fc4494422d7048fbd088cd7fd5535
Nataliya Sashnikova <sashnat@yahoo.com>
2018-07-25 17:22:39 +0300

    dictionary+function+pandas

---
+++ wallpaper_rolls.py
@@ -0,0 +1,12 @@
+# coded by Nataliya Sashnikova
+# Расчет рулонов обоев
+
+
+import math
+from functools import reduce
+import pandas as pd
+import numpy as np
+
+h_roll = float(input('высота (длина) рулона/ roll  height: '))
+w_roll = float(input('ширина рулона/ roll width: '))
+
